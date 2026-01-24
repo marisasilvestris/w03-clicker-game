e42b668ba0660c80328af44e11219a98df3e040f
Marisa Silvestris <marisasilvestris@gmail.com>
2026-01-24 02:41:02 +0000

    bonus upgrades pt.1

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # w03-clicker-game
+
+> emoji toolbox  
+> 🔴🟠🟢
+
+## Requirements checklist
+
+🟢 Fetch upgrade data from the provided API and at least one upgrade from the API update the cookie count.  
+🟠 Ensure that functions are used effectively to keep code organised and reusable.  
+🟢 Implement event listeners to handle user interactions.  
+🟢 Use local storage to save and restore the cookie count and relevant game information.  
+🟢 Use setInterval to increment the cookie count and manage the game state each second. Managing the game state includes saving progress and updating the DOM.
+
+### Stretch requirements
+
+🏹 Consolidate upgrade management by managing all upgrades in a single function.  
+🏹 Improve UX with animations, sound effects, or other visual effects.  
+🏹 Fantastic use of README to provide important information such as a description of the project, how to deploy and other app information.  
+🏹 Implement error handling using try/catch.  
+🏹 Create a menu for users to adjust game options like sound effects or display preferences.
+
+## Reflection
+
+> tl;dr
+
+## To improve
+
+marisa 🐈‍⬛
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log(`meow!`);
+console.log(`meow! 🐈‍⬛`);
 
 const clickerButton = document.getElementById(`clickerButton`);
 const cookieCountDisplay = document.getElementById(`cookieCounter`);
@@ -6,16 +6,60 @@ const clrBtn = document.getElementById(`clrBtn`);
 const sideViewBtn = document.getElementById(`sideViewBtn`);
 const sideView = document.getElementById(`sideView`);
 const shopList = document.getElementById(`shopList`);
+const shopList2 = document.getElementById(`shopList2`);
 
 const cookiesUpSmall = document.getElementById(`cookiesUpSmall`);
 const cookiesUp = document.getElementById(`cookiesUp`);
 const powerUp = document.getElementById(`powerUp`);
 const autoUp = document.getElementById(`autoUp`);
 
+const upgradeImgList = {
+  [`Auto-Clicker`]: `./img/bongocat.jpg`,
+  [`Enhanced Oven`]: `./img/bongocat.jpg`,
+  [`Cookie Farm`]: `./img/bongocat.jpg`,
+  [`Robot Baker`]: `./img/bongocat.jpg`,
+  [`Cookie Factory`]: `./img/bongocat.jpg`,
+  [`Magic Flour`]: `./img/bongocat.jpg`,
+  [`Time Machine`]: `./img/bongocat.jpg`,
+  [`Quantum Oven`]: `./img/bongocat.jpg`,
+  [`Alien Technology`]: `./img/bongocat.jpg`,
+  [`Interdimensional Baker`]: `./img/bongocat.jpg`,
+};
+const bonusUpgradeList = [
+  {
+    id: 1,
+    name: "Finger Training",
+    cost: 1000,
+    increase: 1,
+    img: `./img/upgrades/bonus1.png`,
+  },
+  {
+    id: 2,
+    name: "Reinforced Mouse Button",
+    cost: 100000,
+    increase: 10,
+    img: `./img/upgrades/bonus2.png`,
+  },
+  {
+    id: 3,
+    name: "A Gun Made Of Clicking",
+    cost: 10000000,
+    increase: 100,
+    img: `./img/upgrades/bonus3.png`,
+  },
+  {
+    id: 4,
+    name: "Frankly Unfriendly Amounts Of Clicks",
+    cost: 1000000000,
+    increase: 10000,
+    img: `./img/upgrades/bonus4.png`,
+  },
+];
+
 let cookieCount;
 let clickPower;
 let autoClickPower;
-let upgradeList;
+let upgradeList = {};
 
 function loadStats() {
   cookieCount = JSON.parse(localStorage.getItem(`cookieCount`)) || 0;
@@ -23,59 +67,72 @@ function loadStats() {
   autoClickPower = JSON.parse(localStorage.getItem(`autoClickPower`)) || 0;
   upgradeList = JSON.parse(localStorage.getItem(`upgradeList`)) || {};
 }
+
 function saveStats() {
   localStorage.setItem(`cookieCount`, cookieCount);
   localStorage.setItem(`clickPower`, clickPower);
   localStorage.setItem(`autoClickPower`, autoClickPower);
   const upgradeJSON = JSON.stringify(upgradeList);
   localStorage.setItem(`upgradeList`, upgradeJSON);
-
-  // console.log(
-  //   `saved stats! cookies: ${cookieCount}, click power: ${clickPower}, autoclick power: ${autoClickPower}`,
-  //   upgradeList,
-  // );
 }
+
 function cookieUpdate(c) {
   cookieCount = cookieCount + c;
   cookieCountDisplay.textContent = `${cookieCount}`;
 }
 
-// id
-// name
-// cost
-// increase
 async function gameInit() {
-  async function fetchUpgrades(u) {
-    const response = await fetch(u);
+  function buildButton(e, list) {
+    const listItem = document.createElement(`li`);
+    listItem.innerHTML = `<div class="shop-item" aria-label="button">
+                <img class="item-img" src="${upgradeImgList[e.name] || [e.img]}" />
+                <p class="item-name">Name:${e.name}</p>
+                <p class="item-cost">Cost:${e.cost}</p>
+                <p class="item-increase">Increase:${e.increase}</p>
+                <p class="item-count">Have:${upgradeList[e.name]}</p>
+              </div>`; // i got supremely lazy here but i'll fix it one day!
+    listItem.addEventListener(`click`, () => {
+      if (cookieCount >= e.cost) {
+        cookieUpdate(-e.cost); // spend cookies
+        autoClickPower = autoClickPower + e.increase;
+        console.log(e.increase);
+
+        upgradeList[e.name]++;
+        listItem.innerHTML = `<div class="shop-item" aria-label="button">
+                <img class="item-img" src="./img/ui/yarn.png" />
+                <p class="item-name">Name:${e.name}</p>
+                <p class="item-cost">Cost:${e.cost}</p>
+                <p class="item-increase">Increase:${e.increase}</p>
+                <p class="item-count">Have:${upgradeList[e.name]}</p>
+              </div>`; // doubly lazy!
+      } else {
+        console.log(`you need ${e.cost} cookies!`);
+      }
+    });
+    list.appendChild(listItem);
+  }
+  async function fetchUpgrades(url) {
+    const response = await fetch(url);
     const jsonData = await response.json();
     jsonData.forEach((e) => {
       if (!upgradeList[e.name]) {
         upgradeList[e.name] = 0;
       }
-      const listItem = document.createElement(`li`);
-      const listItemBtn = document.createElement(`button`);
-      listItemBtn.classList = `shop-item`;
-      listItemBtn.textContent = `${upgradeList[e.name]} ${e.name}. cost:${e.cost}. cps increase:${e.increase}`;
-      listItemBtn.addEventListener(`click`, () => {
-        if (cookieCount >= e.cost) {
-          cookieUpdate(-e.cost);
-          autoClickPower = autoClickPower + e.increase;
-          upgradeList[e.name]++;
-          listItemBtn.textContent = `${upgradeList[e.name]} ${e.name}. cost:${e.cost}. cps increase:${e.increase}`;
-        } else {
-          console.log(`you need ${e.cost} cookies!`);
-        }
-      });
-      listItem.appendChild(listItemBtn);
-      shopList.appendChild(listItem);
+      buildButton(e, shopList);
     });
   }
-  fetchUpgrades(`https://cookie-upgrade-api.vercel.app/api/upgrades`);
+  async function fetchBonusUpgrades(array) {
+    array.forEach((e) => {
+      if (!upgradeList[e.name]) {
+        upgradeList[e.name] = 0;
+      }
+      buildButton(e, shopList2);
+    });
+  }
 
   clickerButton.addEventListener(`click`, () => {
     cookieUpdate(clickPower);
   });
-
   sideViewBtn.addEventListener(`click`, () => {
     if (getComputedStyle(sideView).display === `none`) {
       sideView.style.display = `flex`;
@@ -83,7 +140,6 @@ async function gameInit() {
       sideView.style.display = `none`;
     }
   });
-
   clrBtn.addEventListener(`click`, () => {
     cookieCount = 0;
     clickPower = 1;
@@ -112,8 +168,11 @@ async function gameInit() {
     powerUp.textContent = `clickpwr+100: ${clickPower || 0}`;
     autoUp.textContent = `autopwr+10: ${autoClickPower || 0}`;
   }
+
+  fetchUpgrades(`https://cookie-upgrade-api.vercel.app/api/upgrades`);
   cheatBtns();
   loadStats();
+  fetchBonusUpgrades(bonusUpgradeList);
 }
 function gameUpdate() {
   function cheatBtns() {
